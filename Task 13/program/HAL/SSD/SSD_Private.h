5cbd7f545ecc3639660db002c13496b9281b7176
Sara Hussien <sarahussien2005@gmail.com>
2025-08-02 06:44:03 +0300

    Task 13

---
+++ Task 13/program/HAL/SSD/SSD_Private.h
@@ -0,0 +1,27 @@
+/* File Gaurd by ifndef & endif */
+#ifndef SEVEN_SEGMENT_PRIVATE_H
+#define SEVEN_SEGMENT_PRIVATE_H
+
+/* Private Macros */
+
+/* 7-Segment Common Anode macros */
+                  //0b1gfedcba
+//#define ZERO        0b11000000
+//#define ONE         0b11111001
+//#define TWO         0b10100100
+//#define THREE       0b10110000
+//#define FOUR        0b10011001
+//#define FIVE        0b10010010
+//#define SIX         0b10000010
+//#define SEVEN       0b11111000
+//#define EIGHT       0b10000000
+//#define NINE        0b10010000
+//#define A           0b10001000
+//#define B           0b10000000
+//#define C           0b11000110
+//#define D           0b11000000
+//#define E           0b10000110
+//#define F           0b10001110
+
+
+#endif
